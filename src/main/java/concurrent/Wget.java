bafeeea3cc42a96b5f0962b44bf1df811cf19475
JavAndy58 <digital.designs.russia@gmail.com>
2023-03-23 17:54:05 +0300

    4. Режим ожидания. [#231217]

---
+++ src/main/java/concurrent/Wget.java
@@ -0,0 +1,21 @@
+package concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int index = 0; index <= 100; index++) {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + index + "%");
+
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
