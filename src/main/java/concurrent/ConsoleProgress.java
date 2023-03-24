ddbc14997f37a7188681807b12fc51ffeb6fa60e
JavAndy58 <digital.designs.russia@gmail.com>
2023-03-24 13:01:18 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        var process = new char[] {'-', '\\', '|', '/'};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r load: " + process[count++]);
+                if (count == process.length) {
+                    count = 0;
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        try {
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
